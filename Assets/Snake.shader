9cc3e0e58aa2669f0b4ddd479e27513c42be1031
Klemen Brajkovič <klemen.brajkovic@gmail.com>
2016-02-27 14:48:12 +0100

    All vertices are now loaded in the mesh in advance. Mesh movement is done (still need to implement rotations and tail follow)

---
+++ Assets/Snake.shader
@@ -0,0 +1,48 @@
+﻿Shader "d3cr1pt0r/Snake" {
+	Properties{
+		_Color("Color", Color) = (1.0,1.0,1.0,1.0)
+	}
+	SubShader {
+		Tags { "Queue" = "Transparent" }
+		Pass {
+			Blend SrcAlpha OneMinusSrcAlpha
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			uniform half4 _Color;
+
+			struct vertexInput {
+				half4 vertex : POSITION;
+				float3 normal : NORMAL;
+				half2 texcoord : TEXCOORD0;
+			};
+
+			struct vertexOutput {
+				half4 pos : SV_POSITION;
+				half2 tex : TEXCOORD0;
+				float4 posWorld : TEXCOORD1;
+				float3 normalDir : TEXCOORD2;
+			};
+
+			vertexOutput vert(vertexInput v)
+			{
+				vertexOutput o;
+
+				o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.tex = v.texcoord;
+				o.posWorld = mul(_Object2World, v.vertex);
+				o.normalDir = normalize(mul(float4(v.normal, 0.0), _World2Object).xyz);
+
+				return o;
+			}
+
+			fixed4 frag(vertexOutput i) : COLOR
+			{
+				return float4(_Color.rgb, _Color.a);
+			}
+			ENDCG
+		}
+	}
+}
--- Assets/SnakeMesh.cs
+++ Assets/SnakeMesh.cs
@@ -5,6 +5,13 @@ using System;
 
 public class SnakeMesh : MonoBehaviour {
 
+    [SerializeField]
+    private float MeshWidth;
+    [SerializeField]
+    private int NumVertices;
+    [SerializeField]
+    private float UpdateFrequency;
+
     private MeshFilter meshFilter;
     private Mesh mesh;
     private List<Vector3> vertices;
@@ -12,120 +19,101 @@ public class SnakeMesh : MonoBehaviour {
     private List<Vector2> uvs;
     private List<int> triangles;
 
+    private int TimesExtended;
+    private float MoveDistance;
+
 	void Start () {
-        mesh = new Mesh();
-        vertices = new List<Vector3>();
-        normals = new List<Vector3>();
-        uvs = new List<Vector2>();
-        triangles = new List<int>();
-
-        meshFilter = GetComponent<MeshFilter>();
-        meshFilter.mesh = mesh;
-
-        vertices.Add(new Vector3(0, 0, 0));
-        vertices.Add(new Vector3(1, 0, 0));
-        vertices.Add(new Vector3(0, 1, 0));
-        vertices.Add(new Vector3(1, 1, 0));
-        vertices.Add(new Vector3(0, 1, 0));
-        vertices.Add(new Vector3(1, 1, 0));
-
-        Vector3 last_1 = vertices[vertices.Count - 1];
-        Vector3 last_2 = vertices[vertices.Count - 2];
-
-        normals.Add(-Vector3.forward);
-        normals.Add(-Vector3.forward);
-        normals.Add(-Vector3.forward);
-        normals.Add(-Vector3.forward);
-        normals.Add(-Vector3.forward);
-        normals.Add(-Vector3.forward);
-
-        uvs.Add(new Vector2(0, 0));
-        uvs.Add(new Vector2(1, 0));
-        uvs.Add(new Vector2(0, 1));
-        uvs.Add(new Vector2(1, 1));
-        uvs.Add(new Vector2(1, 1));
-        uvs.Add(new Vector2(1, 1));
-
-        triangles.Add(0);
-        triangles.Add(2);
-        triangles.Add(1);
-        triangles.Add(2);
-        triangles.Add(3);
-        triangles.Add(1);
-
-        triangles.Add(2);
-        triangles.Add(4);
-        triangles.Add(3);
-        triangles.Add(4);
-        triangles.Add(5);
-        triangles.Add(3);
-
-        mesh.vertices = vertices.ToArray();
-        mesh.normals = normals.ToArray();
-        mesh.uv = uvs.ToArray();
-        mesh.triangles = triangles.ToArray();
+        TimesExtended = 0;
+        GenerateInitialMesh(NumVertices);
     }
 
     void Update()
     {
-        Vector3[] v = mesh.vertices;
-
-        v[v.Length - 1] += new Vector3(0, 0.01f, 0);
-        v[v.Length - 2] += new Vector3(0, 0.01f, 0);
-
-        mesh.vertices = v;
-        mesh.RecalculateBounds();
-
-        // Add 2 more vertices to the mesh
-        if (GetVertexDistance() >= 1.0f)
-        {
-            ExtendMesh();
-        }
+		MoveMesh(new Vector3(UnityEngine.Random.Range(-0.05f, 0.05f), 0.01f, 0f));
     }
 
-    void ExtendMesh()
-    {
-        Vector3[] v = mesh.vertices;
-        Vector3[] n = mesh.normals;
-        Vector2[] u = mesh.uv;
-        int[] t = mesh.triangles;
+	void GenerateInitialMesh(int numVerticies)
+	{
+		mesh = new Mesh();
+		vertices = new List<Vector3>(numVerticies);
+		normals = new List<Vector3>(numVerticies);
+		uvs = new List<Vector2>(numVerticies);
+		triangles = new List<int>();
 
-        Array.Resize(ref n, n.Length + 2);
-        Array.Resize(ref u, u.Length + 2);
-        Array.Resize(ref t, t.Length + 6);
+		meshFilter = GetComponent<MeshFilter>();
+		meshFilter.mesh = mesh;
 
-        n[n.Length - 2] = -Vector3.forward;
-        n[n.Length - 1] = -Vector3.forward;
+		// Create the first quad
+		vertices.Add(new Vector3(0, 0, 0));
+		vertices.Add(new Vector3(MeshWidth, 0, 0));
+		vertices.Add(new Vector3(0, 0, 0));
+		vertices.Add(new Vector3(MeshWidth, 0, 0));
 
-        u[u.Length - 2] = new Vector2(1, 1);
-        u[u.Length - 1] = new Vector2(1, 1);
+		normals.Add(-Vector3.forward);
+		normals.Add(-Vector3.forward);
+		normals.Add(-Vector3.forward);
+		normals.Add(-Vector3.forward);
 
-        t[t.Length - 6] = t[t.Length - 13] + 2;
-        t[t.Length - 5] = t[t.Length - 12] + 1;
-        t[t.Length - 4] = t[t.Length - 11] + 2;
-        t[t.Length - 3] = t[t.Length - 10] + 2;
-        t[t.Length - 2] = t[t.Length - 9] + 1;
-        t[t.Length - 1] = t[t.Length - 8] + 2;
+		uvs.Add(new Vector2(0, 0));
+		uvs.Add(new Vector2(1, 0));
+		uvs.Add(new Vector2(0, 1 / NumVertices));
+		uvs.Add(new Vector2(1, 1 / NumVertices));
 
-        // vertices
-        Vector3 v1 = v[v.Length - 1];
-        Vector3 v2 = v[v.Length - 2];
+		triangles.Add(0);
+		triangles.Add(2);
+		triangles.Add(1);
 
-        Array.Resize(ref v, v.Length + 2);
+		triangles.Add(2);
+		triangles.Add(3);
+		triangles.Add(1);
 
-        v[v.Length - 2] = v1;
-        v[v.Length - 1] = v2;
+		// Create vertex buffer in advance
+		for (int i=0; i < numVerticies - 4; i++)
+		{
+			if (i < numVerticies / 2)
+			{
+				vertices.Add(new Vector3(0, 0, 0));
+				vertices.Add(new Vector3(MeshWidth, 0, 0));
 
-        mesh.vertices = v;
-        mesh.normals = n;
-        mesh.uv = u;
-        mesh.triangles = t;
-        mesh.RecalculateBounds();
-    }
+				normals.Add(-Vector3.forward);
+				normals.Add(-Vector3.forward);
 
-    float GetVertexDistance()
+				uvs.Add(new Vector2(1, 1));
+				uvs.Add(new Vector2(1, 1));
+
+				triangles.Add(triangles[triangles.Count - 6] + 2);
+				triangles.Add(triangles[triangles.Count - 6] + 2);
+				triangles.Add(triangles[triangles.Count - 6] + 2);
+
+				triangles.Add(triangles[triangles.Count - 6] + 2);
+				triangles.Add(triangles[triangles.Count - 6] + 2);
+				triangles.Add(triangles[triangles.Count - 6] + 2);
+			}
+		}
+
+		mesh.vertices = vertices.ToArray();
+		mesh.normals = normals.ToArray();
+		mesh.uv = uvs.ToArray();
+		mesh.triangles = triangles.ToArray();
+	}
+
+    void MoveMesh(Vector3 dir)
     {
         Vector3[] vertices = mesh.vertices;
-        return Vector3.Distance(vertices[vertices.Length - 1], vertices[vertices.Length - 3]);
+
+        for(int i=vertices.Length - 1; i > 3 + TimesExtended * 2; i--)
+        {
+            vertices[i] += dir;
+        }
+
+        MoveDistance += dir.magnitude;
+        if (MoveDistance >= UpdateFrequency)
+        {
+            MoveDistance = 0;
+            TimesExtended++;
+        }
+
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
     }
 }
