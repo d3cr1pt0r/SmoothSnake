a090d35ec016629fdd9e16da4978d0bce81a550a
Klemen Brajkovič <klemen.brajkovic@gmail.com>
2016-02-28 11:52:31 +0100

    SnakeMesh updated (snake movement works)

--- Assets/SnakeMesh.cs
+++ Assets/SnakeMesh.cs
@@ -6,11 +6,17 @@ using System;
 public class SnakeMesh : MonoBehaviour {
 
     [SerializeField]
-    private float MeshWidth;
+    private float MeshWidth = 1.0f;
     [SerializeField]
-    private int NumVertices;
+    private int NumVertices = 1000;
     [SerializeField]
-    private float UpdateFrequency;
+    private float UpdateFrequency = 0.25f;
+	[SerializeField]
+	private float TurnAngle = 0.5f;
+	[SerializeField]
+	private float DistanceFactor = 100.0f;
+	[SerializeField]
+	private float SnakeLength = 10.0f;
 
     private MeshFilter meshFilter;
     private Mesh mesh;
@@ -29,7 +35,12 @@ public class SnakeMesh : MonoBehaviour {
 
     void Update()
     {
-		MoveMesh(new Vector3(UnityEngine.Random.Range(-0.05f, 0.05f), 0.01f, 0f));
+		if (Input.GetKey(KeyCode.A))
+			MoveMesh(TurnAngle, DistanceFactor);
+		else if (Input.GetKey(KeyCode.D))
+			MoveMesh(-TurnAngle, DistanceFactor);
+		else
+			MoveMesh(0, DistanceFactor);
     }
 
 	void GenerateInitialMesh(int numVerticies)
@@ -97,16 +108,29 @@ public class SnakeMesh : MonoBehaviour {
 		mesh.triangles = triangles.ToArray();
 	}
 
-    void MoveMesh(Vector3 dir)
+	void MoveMesh(float angle, float distance)
     {
+		float moveDistance = 0;
         Vector3[] vertices = mesh.vertices;
+		Vector3[] lastTwoFrozenVertices = GetLastTwoFrozenVertices();
 
-        for(int i=vertices.Length - 1; i > 3 + TimesExtended * 2; i--)
+		Vector3 midPoint = (lastTwoFrozenVertices[0] + lastTwoFrozenVertices[1]) / 2;
+
+        for(int i=vertices.Length - 1; i > 1 + TimesExtended * 2; i-=2)
         {
-            vertices[i] += dir;
+			vertices[i-1] = RotatePointAroundPivot(vertices[i-1], midPoint, new Vector3(0, 0, angle));
+			vertices[i] = RotatePointAroundPivot(vertices[i], midPoint, new Vector3(0, 0, angle));
+
+			Vector3 pdir = Vector3.Normalize(vertices[i-1] - vertices[i]);
+			Vector3 newPDir = Vector3.Normalize(Quaternion.Euler(0, 0, -90) * pdir);
+
+			vertices[i-1] += newPDir / distance;
+			vertices[i] += newPDir / distance;
+
+			moveDistance = (newPDir / distance).magnitude;
         }
 
-        MoveDistance += dir.magnitude;
+		MoveDistance += moveDistance;
         if (MoveDistance >= UpdateFrequency)
         {
             MoveDistance = 0;
@@ -116,4 +140,28 @@ public class SnakeMesh : MonoBehaviour {
         mesh.vertices = vertices;
         mesh.RecalculateBounds();
     }
+
+	Vector3[] GetLastTwoFrozenVertices() {
+		if(TimesExtended * 2 + 1 < mesh.vertices.Length)
+			return new Vector3[2]{ mesh.vertices[TimesExtended * 2], mesh.vertices[TimesExtended * 2 + 1] };
+		else 
+			return new Vector3[2]{ mesh.vertices[mesh.vertices.Length-1], mesh.vertices[mesh.vertices.Length-2] };
+	}
+
+	Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles) {
+		Vector3 dir = point - pivot;
+		dir = Quaternion.Euler(angles) * dir;
+		point = dir + pivot;
+		return point;
+	}
+
+	float GetMeshLength() {
+		Vector3[] vertices = mesh.vertices;
+		float distance = 0;
+
+		for(int i=0; i < vertices.Length - 2; i+=2)
+			distance += Vector3.Distance(vertices[i], vertices[i+2]);
+
+		return distance;
+	}
 }
